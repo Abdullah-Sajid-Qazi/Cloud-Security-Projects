424d0108261b9dfb5a5f4e39a94148e39c587e97
Abdullah Sajid Qazi <abdullahsajidqazi.o@gmail.com>
2026-02-01 22:12:17 +0500

    Add files via upload

---
+++ Secure VPC Using Infrastructure as Code/terraform/routing_private.tf
@@ -0,0 +1,23 @@
+# Private route tables (one per AZ)
+resource "aws_route_table" "private" {
+  count  = 2
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this[count.index].id
+  }
+
+  tags = {
+    Name      = "${var.project_name}-private-rt-${count.index + 1}"
+    Tier      = "private"
+    ManagedBy = "terraform"
+  }
+}
+
+# Associate private route tables with private subnets
+resource "aws_route_table_association" "private" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
